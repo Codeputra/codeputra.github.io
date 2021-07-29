24ce1b2c4cf3c0fb005b1617c060a91027301053
Putra Agustiansyah <putraagustiansyah000@gmail.com>
2021-07-29 21:32:27 +0700

    update bootstrap ke versi 5.0.2

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const sMerah = document.querySelector("input[name=sMerah]");
-
-sMerah.addEventListener("input", function(){
-    const r = sMerah.value;
-    document.body.style.backgroundColor = "rgb( "+ r +", 100, 100)";
-});
